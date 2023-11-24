281cea54768f32a6e9f118b351de45be499fdd0a
Brent Reynolds <brentsreynolds5@gmail.com>
2023-11-24 18:21:43 -0500

    Init

---
+++ StoryClient.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class StoryClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(SERVER_ADDRESS, PORT);
+             Scanner serverInput = new Scanner(socket.getInputStream());
+             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+             Scanner userInput = new Scanner(System.in)) {
+
+            // Read and print the initial story
+            String initialStory = serverInput.nextLine();
+            System.out.println("Current Story:\n" + initialStory);
+
+            // Allow the player to contribute to the story
+            while (true) {
+                String message = serverInput.nextLine();
+                String turnIndicator = "";
+                // Print the message from the server (Your turn or the updated story)
+                if(message.equals("Your turn: ") || message.equals("Waiting for other players...")) {
+                    turnIndicator = message;
+                    if(turnIndicator.equals("Your turn: ")){
+                        System.out.print(turnIndicator);
+                    }
+                    //System.out.print("turn indicator: " + turnIndicator);
+                    if (turnIndicator.equals("Your turn: ")) {
+                    System.out.print("Enter your sentence: ");
+                    String userSentence = userInput.nextLine();
+                    output.println(userSentence);
+                    }
+                    else {
+                    String updatedStory = serverInput.nextLine();
+                    System.out.println(updatedStory);
+                }
+                }
+                else {
+                    if(message != "Waiting for other players...") {
+                        String updatedStory = message;
+                        System.out.println(updatedStory);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ StoryServer.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class StoryServer {
+    private static final int PORT = 12345;
+    private static List<PrintWriter> clientWriters = new ArrayList<>();
+    private static String currentStory = "";
+    private static int currentPlayerIndex = 0;
+    private static Semaphore turnSemaphore = new Semaphore(1);
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server is running on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private Scanner input;
+        private PrintWriter output;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                input = new Scanner(clientSocket.getInputStream());
+                output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // Add the client's PrintWriter to the list
+                synchronized (clientWriters) {
+                    clientWriters.add(output);
+                }
+
+                // Send the current story to the new client
+                output.println(currentStory);
+
+                while (true) {
+                    // Acquire the semaphore to control the turn
+                    turnSemaphore.acquire();
+
+                    // Notify the current player and wait for their turn
+                    int playerIndex = clientWriters.indexOf(output);
+                    if (playerIndex == currentPlayerIndex) {
+                        output.println("Your turn: ");
+                        String clientInput = input.nextLine();
+
+                        // Update the current story
+                        currentStory += clientInput + "\n";
+
+                        // Switch to the next player
+                        currentPlayerIndex = (currentPlayerIndex + 1) % clientWriters.size();
+
+                        // Send the updated story to all clients
+                        synchronized (clientWriters) {
+                            for (PrintWriter writer : clientWriters) {
+                                writer.println(currentStory);
+                            }
+                        }
+                    } else {
+                        //output.println("Waiting for other players...");
+                    }
+
+                    // Release the semaphore to allow the next player to take a turn
+                    turnSemaphore.release();
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                // Remove the client's PrintWriter when they disconnect
+                synchronized (clientWriters) {
+                    clientWriters.remove(output);
+                }
+            }
+        }
+    }
+}
+
